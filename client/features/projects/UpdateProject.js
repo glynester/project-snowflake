bd5c48f5f62d23811fefe8c0a9090d8330c450ef
david anderson <d.a.m.anderson@gmail.com>
2016-12-07 15:29:33 +0000

    added commented out template and route to handle project update, need to get project id first

---
+++ client/features/projects/UpdateProject.js
@@ -0,0 +1,7 @@
+// Template.UpdateProfile.helpers({
+//   getProject(){
+//     var currentUser = Meteor.userId();
+//     var userProfile = Profiles.findOne({created_by: currentUser});
+//     return userProfile;
+//   },
+// });
