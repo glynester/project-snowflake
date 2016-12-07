56ffeac9bfab36995adb9192a34eca5867247917
amidabrian51 <briankaty1@me.com>
2016-12-07 15:08:34 +0000

    Added Projects.js in collections folder

---
+++ collections/Projects.js
@@ -0,0 +1,51 @@
+Projects = new Mongo.Collection('projects')
+
+ProjectsSchema = new SimpleSchema({
+  created_by: {
+    type: String,
+    label: 'Created by:',
+    optional: true,
+    autoValue: function() {
+      return this.userId;
+    },
+    autoform: {
+      type: 'hidden'
+    }
+  },
+  skills: {
+    type: String,
+    label: 'Skills:',
+  },
+  location: {
+    type: String,
+    label: 'Location:'
+  },
+  description: {
+    type: String,
+    label: 'Description:',
+  },
+  createdAt: {
+    type: Date,
+    label: "Created At",
+    autoValue: function() {
+    return new Date()
+    },
+    autoform: {
+          type: "hidden"
+      }
+    },
+    date: {
+      type: Date,
+      label: "Date of Project"
+    },
+    minPeople: {
+      type: Number,
+      label: 'Minumum People Required'
+    },
+    maxPeople: {
+      type: Number,
+      label: 'Maximum People Allowed'
+    }
+});
+
+Projects.attachSchema(ProjectsSchema);
