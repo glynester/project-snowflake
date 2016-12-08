7f663219d317d9a9dcc34616693d6ece3b4b03fe
david anderson <d.a.m.anderson@gmail.com>
2016-12-08 12:45:59 +0000

    can only see delete and update buttons on  own projects

--- client/features/projects/ProjectSingle.js
+++ client/features/projects/ProjectSingle.js
@@ -2,14 +2,20 @@ Template.ProjectSingle.helpers({
     project: () => {
         var id = FlowRouter.getParam('id');
         return Projects.findOne({_id: id});
+    },
+    currentUserProject: () => {
+        var currentUserId = Meteor.userId();
+        var projectId = FlowRouter.getParam('id');
+        var currentProject = Projects.findOne({_id: projectId});
+        return currentProject.created_by === currentUserId;
     }
-
 });
 
 Template.ProjectSingle.events({
   'click #delete-project' (){
     var id = FlowRouter.getParam('id');
-    Projects.remove({_id: id})
-  }
+    Projects.remove({_id: id});
+    FlowRouter.go('view-projects');
 
+  }
 });
