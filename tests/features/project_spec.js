8aaba9cb753e64e61e3c9615997ec86c0a20cdaa
glynester <glynester@hotmail.com>
2016-12-09 12:43:47 +0000

    Project test

--- tests/features/project_spec.js
+++ tests/features/project_spec.js
@@ -13,12 +13,14 @@ describe('Add Project', function() {
     browser.url('localhost:3000/project/new');
     browser.setValue( '[name="location"]', 'Twickenham' );
     browser.setValue( '[name="description"]', 'test project' );
-    //browser.setValue( '[name="date"]', '20/12/2016' );
+    // browser.setValue( '[name="date"]', new Date().getTime() );
     browser.setValue( '[name="minPeople"]', '4' );
     browser.setValue( '[name="maxPeople"]', '10' );
     browser.setValue( '[name="skills.0.skill"]', 'leaf blowing' );
     browser.click('.btn-primary');
-    expect(browser.getText('.login-link-text')).to.equal("test project");
+    browser.url('localhost:3000/projects');
+    expect(browser.getText('.projects')).to.equal("test project");
+
     //expect(browser.getUrl()).to.equal("http://localhost:3000/project/new");
   });
 });
