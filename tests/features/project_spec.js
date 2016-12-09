f933c6e3146168464ae2ad8b00549e74a93cf9f6
amidabrian51 <briankaty1@me.com>
2016-12-09 15:25:47 +0000

    Added test for update of project

--- tests/features/project_spec.js
+++ tests/features/project_spec.js
@@ -28,18 +28,22 @@ describe('Project', function() {
 
   it('amends a project @watch', function () {
     // signIn();
-    browser.url('localhost:3000/project/new');
-    browser.setValue( '[name="location"]', 'Twickenhammy' );
-    browser.setValue( '[name="description"]', 'test projecty' );
-    // browser.setValue( '[name="date"]', new Date() );
-    // browser.setValue( '[name="date"]', '28/12/2016, 10:30' );
-    browser.setValue( '[name="minPeople"]', '4' );
-    browser.setValue( '[name="maxPeople"]', '10' );
-    browser.setValue( '[name="skills.0.skill"]', 'leaf blowing' );;
-    browser.submitForm('#insertProjectForm');
     var getProject = server.execute( function() {
             return Projects.findOne( { description: 'test projecty' } );
           });
-          expect ( getProject.description ).to.equal( 'test projecty');
+          var projectId = getProject._id;
+    browser.url('localhost:3000/project/update/' + projectId);
+    browser.setValue( '[name="location"]', 'Hounslow' );
+    browser.setValue( '[name="description"]', 'XYZ' );
+    // browser.setValue( '[name="date"]', new Date() );
+    // browser.setValue( '[name="date"]', '28/12/2016, 10:30' );
+    browser.setValue( '[name="minPeople"]', '5' );
+    browser.setValue( '[name="maxPeople"]', '101' );
+    browser.setValue( '[name="skills.0.skill"]', 'singing' );;
+    browser.submitForm('#updateProjectForm');
+    var getProject = server.execute( function() {
+            return Projects.findOne( { description: 'XYZ' } );
+          });
+          expect ( getProject.description ).to.equal( 'XYZ');
   });
 });
