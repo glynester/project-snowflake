1656e084a630789e267e8d1d2f7feeafc5fdb08b
Liam Taylor <liam.taylor888@googlemail.com>
2016-12-09 16:12:27 +0000

    1) Added logic so that clicking on 'profile' routes to /profile/new or /profile depending on whether a profile exists 2) Added event to route to homepage once profile has been created

--- client/features/profile/Profile.js
+++ client/features/profile/Profile.js
@@ -32,6 +32,13 @@ Template.NewProfile.helpers({
   },
 });
 
+Template.NewProfile.events({
+  'submit #insertProfileForm'(event) {
+    FlowRouter.go('main');
+  },
+
+});
+
 Template.UpdateProfile.events({
   'submit #updateProfileForm'(event) {
     var currentUser = Meteor.userId();
