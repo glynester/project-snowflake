c3fad24dbb7d95c1e0cf408f35ab6457abdc7c6f
James <bakerje@mac.com>
2016-12-12 12:51:06 +0000

    Added email verification

--- server/main.js
+++ server/main.js
@@ -1,5 +1,17 @@
+
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+if (Meteor.isServer) {
+Meteor.startup(function() {
+  process.env.MAIL_URL='smtp://postmaster%40sandbox467cfd33c95840e2926f51d397112a40.mailgun.org:a402a344997c8b9cc02d637c04e2211c@smtp.mailgun.org:587';
+  Accounts.config({
+    sendVerificationEmail:true
+  });
+  Accounts.emailTemplates.verifyEmail.subject  = function(user) {
+          return 'Please confirm you want to join Snowflake!!';
+  };
+  Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+          return 'Please click on the link to sign up and start doing good! '  + url;
+  };
 });
+}
