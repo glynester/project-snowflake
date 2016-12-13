a181d405bf60908038fd1d3ed6cf539b7e874f5c
Liam Taylor <liam.taylor888@googlemail.com>
2016-12-13 09:54:55 +0000

    A mess

--- client/features/Projects/ProjectsByLocation.js
+++ client/features/Projects/ProjectsByLocation.js
@@ -1,8 +1,18 @@
 Template.ProjectsByLocation.helpers({
   projects: () => {
-    var id = Meteor.userId();
-    var prof = Profiles.findOne({created_by: id});
-    return Projects.find( {} );
+    var projects = Projects.find({date: {$gte: new Date() }});
+    console.log(projects.toArray);
+    locations = []
+    return projects
+    console.log("WWWW")
+    projects.forEach(function (proj) {
+      console.log(proj.location);
+      locations.push(proj.location);
+    });
+    console.log("SOBSOBSOB")
+    console.log(locations)
+
+
   },
   noProjects(){
     var id = Meteor.userId();
@@ -36,13 +46,9 @@ Template.mapmap.helpers({
     console.log(longAndLat[0]);
     if (GoogleMaps.loaded()) {
       var map = {
-        center: new google.maps.LatLng(longAndLat[0],longAndLat[1]),
+        center: new google.maps.LatLng(54.8175053,longAndLat[1]),
         zoom: 8
       };
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(longAndLat[0],longAndLat[1]),
-        map: map
-      });
 
     }
     return map
@@ -50,12 +56,33 @@ Template.mapmap.helpers({
   }
 });
 
-Template.body.onCreated(function() {
+Template.mapmap.onCreated(function() {
+  console.log(Meteor.call('getProjects'));
+  locations = []
+  var allProjects = Projects.find({date: {$gte: new Date() }});
+  console.log (allProjects)
+  console.log(allProjects.count())
+  // allProjects.forEach(function (proj) {
+  //   console.log(proj.location);
+  //   locations.push(proj.location);
+  // });
+  // console.log(locations)
   // We can use the `ready` callback to interact with the map API once the map is ready.
   GoogleMaps.ready('map', function(map) {
+    // var allProjects = Sessions.get('projects');
+    // console.log(allProjects);
     // Add a marker to the map once it's ready
+
     var marker = new google.maps.Marker({
-      position: map.options.center,
+      position: new google.maps.LatLng(54.95268,-1.603411000000051),
+      map: map.instance
+    });
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(54.95268,-1.503411000000051),
+      map: map.instance
+    });
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(52.95268,-0.503411000000051),
       map: map.instance
     });
   });
