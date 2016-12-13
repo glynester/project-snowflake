fb10913bdf66257655081eba2921b19ff20bf543
Liam Taylor <liam.taylor888@googlemail.com>
2016-12-13 10:53:50 +0000

    Maps functionality working on project by location view

--- client/features/Projects/ProjectsByLocation.js
+++ client/features/Projects/ProjectsByLocation.js
@@ -1,18 +1,7 @@
 Template.ProjectsByLocation.helpers({
   projects: () => {
     var projects = Projects.find({date: {$gte: new Date() }});
-    console.log(projects.toArray);
-    locations = []
     return projects
-    console.log("WWWW")
-    projects.forEach(function (proj) {
-      console.log(proj.location);
-      locations.push(proj.location);
-    });
-    console.log("SOBSOBSOB")
-    console.log(locations)
-
-
   },
   noProjects(){
     var id = Meteor.userId();
@@ -33,8 +22,6 @@ Template.ProjectsByLocation.events({
   },
 });
 
-
-
 Template.mapmap.helpers({
   mapOptions(){
 
@@ -43,48 +30,31 @@ Template.mapmap.helpers({
     var location = profile.location;
     var long = location.split(",");
     var longAndLat =  long.map(Number);
-    console.log(longAndLat[0]);
     if (GoogleMaps.loaded()) {
       var map = {
-        center: new google.maps.LatLng(54.8175053,longAndLat[1]),
-        zoom: 8
+        center: new google.maps.LatLng(longAndLat[0],longAndLat[1]),
+        zoom:15
       };
 
     }
     return map
-    console.log(marker)
   }
 });
 
 Template.mapmap.onCreated(function() {
-  console.log(Meteor.call('getProjects'));
-  locations = []
+  var locations = [];
   var allProjects = Projects.find({date: {$gte: new Date() }});
-  console.log (allProjects)
-  console.log(allProjects.count())
-  // allProjects.forEach(function (proj) {
-  //   console.log(proj.location);
-  //   locations.push(proj.location);
-  // });
-  // console.log(locations)
-  // We can use the `ready` callback to interact with the map API once the map is ready.
+  allProjects.forEach(function (proj) {
+    var location = proj.location;
+    locations.push(location);
+  });
   GoogleMaps.ready('map', function(map) {
-    // var allProjects = Sessions.get('projects');
-    // console.log(allProjects);
-    // Add a marker to the map once it's ready
-
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(54.95268,-1.603411000000051),
-      map: map.instance
-    });
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(54.95268,-1.503411000000051),
-      map: map.instance
-    });
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(52.95268,-0.503411000000051),
-      map: map.instance
-    });
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i].split(",")[0], locations[i].split(",")[1]),
+        map: map.instance
+      });
+    };
   });
 });
 
