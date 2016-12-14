787afd38466fe6ba0693397897004687ebcaef7a
Liam Taylor <liam.taylor888@googlemail.com>
2016-12-14 19:21:58 +0000

    fixed typo in hooks.js

--- client/hooks.js
+++ client/hooks.js
@@ -3,6 +3,8 @@ AutoForm.addHooks(['insertProjectForm', 'updateProjectForm'], {
   onSuccess: function(insert, success) {
     console.log("successy!!!")
     FlowRouter.go('view-projects');
+  }
+});
 
 AutoForm.addHooks(['insertProfileForm', 'updateProfileForm'], {
   onSuccess: function(insert, success) {
