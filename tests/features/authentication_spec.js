6d2365b6e0d033b006a0d73b4e36e377d24e6b7e
david anderson <d.a.m.anderson@gmail.com>
2016-12-15 09:53:02 +0000

    tests still not passing

--- tests/features/authentication_spec.js
+++ tests/features/authentication_spec.js
@@ -8,14 +8,14 @@ describe('my module', function () {
 
 
 describe('Running the signUp helper', function() {
-  it('should sign a user up', function () {
+  it('should sign a user up @watch', function () {
     signUp();
     expect(browser.getUrl()).to.equal("http://localhost:3000/main");
   });
 });
 
 describe('Running the logOut helper', function() {
-    it('should log a user out', function () {
+    it('should log a user out @watch', function () {
       logOut();
       expect(browser.getUrl()).to.equal("http://localhost:3000/main");
     });
@@ -24,7 +24,7 @@ describe('Running the logOut helper', function() {
 describe('Signing in', function() {
     it('takes the user to the homepage and displays their email @watch', function () {
       signIn();
-      expect(browser.getText('.login-link-text')).to.equal("hamish.smith@test.com ▾");
+      expect(browser.getText('#login-name-link')).to.equal("hamish.smith@test.com ▾");
     });
 });
 
